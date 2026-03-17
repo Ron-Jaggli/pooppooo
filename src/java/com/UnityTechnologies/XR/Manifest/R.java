0075fc5f26df550fae3d4186b864dedb53cf6b6c
Ron Jaggli <rjaggli7605@gmail.com>
2026-03-17 14:28:56 -0300

    Add files via upload

---
+++ src/java/com/UnityTechnologies/XR/Manifest/R.java
@@ -0,0 +1,7 @@
+package com.UnityTechnologies.XR.Manifest;
+
+/* JADX INFO: loaded from: /storage/emulated/0/Documents/jadec/sources/com.TrassGames.G2Companion/dex-files/0.dex */
+public final class R {
+    private R() {
+    }
+}
